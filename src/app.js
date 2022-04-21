4111fe8f015d0bd1db67a1a90b8ca5ba33042fb3
jayavardhan <jayaedde@gmail.com>
2022-04-21 12:00:28 +0530

    added gps feature

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 const path=require('path')
 const express=require('express')
 const forecast=require('./utils/forecast')
+const address=require('./utils/Converting_co-ordinates-address')
+
+
 
 
 const publicPath=path.join(__dirname,'../public')
@@ -12,7 +15,10 @@ app.set('view engine','hbs')
 app.use(express.static(publicPath))
 
 
+
+
  app.get('',(req,res)=>{
+   
     res.render('index')
 })
 
@@ -24,6 +30,34 @@ app.get('/weather',(req,res)=>{
      }
 })
 
+// app.get('/currentWeather',(req,res)=>{
+//     if(req.query.latitude){
+//         address(req.query.latitude,req.query.longitude,(error,data)=>{
+//             if(data.locality){
+//                 forecast(data.locality,(error,data)=>{
+//                     return res.send
+//                 }
+//             }
+//         })
+//     }
+   
+// })
+app.get('/currentWeather',(req,res)=>{
+    if(req.query.latitude){
+        address(req.query.latitude,req.query.longitude,(error,data)=>{
+            if(data.locality){
+                forecast(data.locality,(error,data)=>{
+                    return res.send(data)
+                })
+            }
+            else{
+                forecast(data.county,(error,data)=>{
+                    return res.send(data)
+                }) 
+            }
+        })
+    }
+})
 
 app.listen(port,()=>{
     console.log("Server is Running!!")
