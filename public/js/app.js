b546c71d9ea0925d1df86b4bd8cbf40ad1c7aba3
jayavardhan <jayaedde@gmail.com>
2022-04-21 12:08:16 +0530

    add gps feature

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,29 @@
+
+
 const adde=document.querySelector('#location')
 document.querySelector('.btn').addEventListener('click',(e)=>{
     e.preventDefault()
     fetch('/weather?address='+adde.value).then((response)=>{
         response.json().then((data)=>{
-            console.log(data)
             document.querySelector('.message').textContent=data.weather
         })
     })
  })
+
+ navigator.geolocation.getCurrentPosition((position)=>{
+   const latitude=position.coords.latitude
+   const longitude=position.coords.longitude
+   console.log(latitude,longitude)
+   fetch(`/currentWeather?latitude=${latitude}&longitude=${longitude}`).then((response)=>{
+    response.json().then((data)=>{
+        document.querySelector('.message').textContent=data.weather
+        console.log("hello")
+    })
+ })  
+ },()=>{
+    alert('Could not get your position')
+})
+
+
 
 
